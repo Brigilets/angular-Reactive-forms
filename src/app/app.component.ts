37f3b2edc48e150a901903f761394325e808bcf7
Brigilets <b.sabutyte@gmail.com>
2023-08-08 10:46:47 +0200

    added custom form validator and implemented different error messages according to the error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +17,16 @@ export class AppComponent implements OnInit {
 
   signUpForm: FormGroup;
 
+  forbiddenUsernames: string[] = ['Chris', 'Anna'];
+  constructor(private formBuilder: FormBuilder) {}
+
   ngOnInit(): void {
     this.signUpForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl(null, Validators.required),
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this),
+        ]),
         email: new FormControl(null, [Validators.required, Validators.email]),
       }),
 
@@ -32,5 +44,13 @@ export class AppComponent implements OnInit {
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signUpForm.get('hobbies')).push(control);
+  }
+
+  // custom validator
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return { nameIsForbidden: true };
+    }
+    return null;
   }
 }
